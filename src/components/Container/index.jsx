995af5619a1a37be12df3266bbb407405fc94776
Sanjiv <sanjiv.samal39@gmail.com>
2024-06-08 16:23:23 +0530

    updates : added window connection for interacting with parent above iframes

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,47 +1,80 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import lightOrdarkColor from "@check-light-or-dark/color";
 import { RiChat1Line } from "react-icons/ri";
 
 const getDataFromChannel = (msg) => {
-  if (typeof msg === "object" && msg) {
-    if (msg.source && msg.source === "huma-chatbot-parent" && msg.data) {
-      const data = msg.data;
+  try {
+    if (msg.source === "huma-chatbot-parent" && msg.data.type === "update") {
+      const data = msg.data.data;
       const obj = {
-        type: "update",
+        type: msg.data.type,
         data: {
-          theme: data.theme || "",
-          avatar: data.avatar || "",
-          name: data.name || "",
+          theme: data.theme,
+          avatar: data.avatar,
+          name: data.name,
         },
       };
-      for (const key in obj) {
-        const val = Boolean(obj[key].trim());
+      for (const key in obj.data) {
+        const val = Boolean(obj.data[key].trim());
         if (!val) {
           delete obj[key];
         }
       }
       return obj;
     }
+    return null;
+  } catch (err) {
+    // console.log(err);
+    return null;
   }
-  return null;
+};
+
+const updateColor = (data) => {
+  try {
+    if (data) {
+      let botData = data.data;
+      let root = document.querySelector(":root");
+      if (root && botData.theme) {
+        const theme = lightOrdarkColor(botData.theme);
+        // console.log(theme);
+        let style = `--theme-color: ${botData.theme}; --font-color: ${
+          theme === "light" ? "black" : "white"
+        } ;`;
+        theme && root.setAttribute("style", style.trim());
+        return true;
+      }
+      return false;
+    }
+  } catch (err) {
+    return false;
+  }
 };
 
 export default function ChatbotContainer() {
-  const channel = new BroadcastChannel("huma-chatbot");
+  const [parent, setParent] = useState(null);
+  const [origin, setOrigin] = useState("");
 
   useEffect(() => {
-    channel.addEventListener("message", (e) => {
-      const data = getDataFromChannel(msg);
-      if (data) {
-        let botData = data.data;
-        let root = document.querySelector(":root");
-        if (root && botData.theme) {
-          const theme = lightOrdarkColor(botData.theme);
-          let style = `--theme-color: ${botData.theme}; --font-color: ${
-            theme === "light" ? "black" : "white"
-          } ;`;
-          root.setAttribute("style", style.trim());
+    window.addEventListener("message", (e) => {
+      try {
+        if (
+          e.origin.trim() &&
+          e.source &&
+          e.data.source === "huma-chatbot-parent"
+        ) {
+          // console.log(e.origin);
+          setParent(e.source);
+          setOrigin(e.origin.trim());
+          // console.log("source updated");
         }
+        const msg = e.data;
+        // console.log(msg);
+        const data = getDataFromChannel(msg);
+        // console.log(data);
+        const updated = updateColor(data);
+        // console.log(`Color was ${!updated ? "not" : ""} updated`);
+      } catch (err) {
+        // console.log("error message");
       }
     });
   }, []);
@@ -57,7 +90,7 @@ export default function ChatbotContainer() {
             value: true,
           },
         };
-        channel.postMessage(message);
+        parent && origin && parent.postMessage(message, origin);
       }}
     >
       <RiChat1Line />
